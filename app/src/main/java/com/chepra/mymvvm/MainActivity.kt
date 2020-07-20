46c8eef59544ce333d1fa801b0cc3e2a28010bce
ehsankolivand <ehsankolivandeh@gmail.com>
2020-07-20 19:55:48 +0430

    i've added libraries ( retrofit & lifecycle & coroutiones )

--- app/src/main/java/com/chepra/mymvvm/MainActivity.kt
+++ app/src/main/java/com/chepra/mymvvm/MainActivity.kt
@@ -2,10 +2,17 @@ package com.chepra.mymvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runBlocking {
+            launch {
+                delay(1000L)
+                Toast.makeText(applicationContext,"it's okey",Toast.LENGTH_LONG).show() }
+        }
     }
 }
